03d97fc50dfb38c53bc0aa3bfe0fa5343c27fe07
Pipe930 <felip.sanchezc@duocuc.cl>
2023-05-10 18:24:44 -0400

    se agrego los modelos a la configuracion en el panel de administrador

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Subscription
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# Register models for panel control
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["username", "email", "is_active", "is_staff", "is_superuser"]
+    list_per_page = 10
+
+admin.site.register(User, UserAdmin)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    ordering = ["username"]
+    list_display = ["username", "email", "mount"]
+
+admin.site.register(Subscription, SubscriptionAdmin)
