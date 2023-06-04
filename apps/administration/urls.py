b64abef7060873351647c5c496cb10697300cc65
Pipe930 <felip.sanchezc@duocuc.cl>
2023-06-03 23:36:36 -0400

    se corrigio la vista login y se creo la vista de panel y la funcionalidad de login

--- apps/administration/urls.py
+++ apps/administration/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from apps.administration.views.login.views import LoginFormView
+from .views.login.views import LoginFormView, LogoutFormView
+from .views.panel.views import PanelView
 
 urlsAdministration = [
-    path("auth/login", LoginFormView.as_view(), name="login")
+    path("auth/login", LoginFormView.as_view(), name="login"),
+    path("auth/logout", LogoutFormView.as_view(), name="logout"),
+    path("dashboard/", PanelView.as_view(), name="panel")
 ]
--- apps/administration/views/login/views.py
+++ apps/administration/views/login/views.py
@@ -1,12 +1,13 @@
-from django.views.generic import CreateView, FormView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.views.generic import  RedirectView
+from django.contrib.auth import  logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import redirect
 import core.settings as setting
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ingrese un username'})
         self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ingrese una contraseña'})
@@ -16,12 +17,33 @@ class LoginFormView(LoginView):
     form_class = CustomAuthenticationForm
     template_name = "login.html"
 
+    def form_valid(self, form, **kwargs):
+        user = form.get_user()
+
+        if not user.is_superuser:
+
+            return redirect('login')
+
+        return super().form_valid(form)
+
     def dispatch(self, request, *args, **kwargs):
+
         if request.user.is_authenticated:
             return redirect(setting.LOGIN_REDIRECT_URL)
+
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
         context['title'] = 'Iniciar Sesion'
+
         return context
+
+class LogoutFormView(RedirectView):
+
+    pattern_name = "login"
+    def dispatch(self, request, *args, **kwargs):
+        logout(request)
+        return super().dispatch(request, *args, **kwargs)
+
